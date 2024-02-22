e0de782e4f97c2220051fce60035f6fe1190301c
bbangjun <lucid9dawn@gmail.com>
2024-02-22 10:37:30 +0900

    feat(index.js): 소환사명#태그라인으로 랭크, 노말별로 10경기씩 결과 검색 로직 구현 완료

--- fightmegg_riot_api/index.js
+++ fightmegg_riot_api/index.js
@@ -11,20 +11,76 @@ const config = await import('./config/config.json', {
     const rAPI = new RiotAPI(XRiotToken);
 
     try {
-        // 소환사 정보 조회
-        const summoner = await rAPI.summoner.getBySummonerName({
-            region: PlatformId.KR,
-            summonerName: "빵준갓",
-        });
 
-        console.log("Summoner:", summoner);
+        // 소환사명과 태그로 puuid 조회
+        // /riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}
+        // Get account by riot id
+        const summoner = await rAPI.account.getByRiotId(
+            {
+              region: PlatformId.ASIA, 
+              gameName: "사랑해 젤리피쉬", 
+              tagLine: "KR1",
+            }
+        );
 
-        const matches = await rAPI.matchV5.getIdsByPuuid({
+        console.log("summoner:", summoner);
+
+        // summoner id로 match id 조회
+        // MATCH-V5 /lol/match/v5/matches/by-puuid/{puuid}/ids
+        // Get a list of match ids by puuid
+        const matchesIds = await rAPI.matchV5.getIdsByPuuid(
+          {
             cluster: PlatformId.ASIA,
             puuid: summoner.puuid,
-        });
+            params: {
+              count: 20
+            },
+          }
+        );
 
-        console.log("Matches:", matches);
+        console.log("MatcheIds:", matchesIds);
+
+
+        let normalMatchIds = [];
+        let rankMatchIds = [];
+
+        // match id로 match 정보 조회하고 랭겜, 일반겜 각각 최대 10개까지 저장
+        // /lol/match/v5/matches/{matchId}
+        // Get a match by match id
+        for (const matchId of matchesIds){
+          
+          if (normalMatchIds.length === 10 && rankMatchIds.length === 10) {
+            break;
+          }
+
+          const matchInformation = await rAPI.matchV5.getMatchById(
+            {
+            cluster: PlatformId.ASIA,
+            matchId: matchId
+            }
+          );
+
+          // console.log("MatcheId:", matchId);
+          // console.log(matchInformation.info.queueId);
+          
+          if (matchInformation.info.queueId === 420){ // rank
+            if(rankMatchIds.length == 10){
+              continue;
+            }
+            rankMatchIds.push(matchId);
+          }
+          else if (matchInformation.info.queueId === 490){ // normal
+            if(normalMatchIds.length == 10){
+              continue
+            }
+            normalMatchIds.push(matchId);
+          }
+          // console.log(`match info about ${matchId}:`, JSON.stringify(matchInformation));
+        }
+
+        console.log("rank game matchId:", rankMatchIds);
+        console.log("normal game matchId:", normalMatchIds);
+
     } catch (error) {
         console.error(error);
     }
